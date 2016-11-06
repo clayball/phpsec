2682d3259b517a23547d805cd966b819f1dce5a8
Clay Wells <cwells@cwells.org>
2016-11-06 09:35:56 -0500

    adding files for our basic starting point.

---
+++ phpsec.php
@@ -0,0 +1,122 @@
+<?php
+
+/******************************************************************************
+ *
+ * phpsec.php
+ *
+ * Clay Wells <cwells (at) cwells (.) org>
+ *
+ * Get PHP tokens and perform static analysis checks that we define (TODO)
+ *
+ * A full list of parser tokens is available at:
+ *
+ * http://php.net/manual/en/tokens.php
+ *
+ * The tokenizer functions provide an interface to the PHP tokenizer embedded
+ * in the Zend Engine. Using these functions you may write your own PHP source
+ * analyzing or modification tools without having to deal with the language
+ * specification at the lexical level.
+ * - source: http://php.net/manual/en/function.token-get-all.php
+ *
+ * token_get_all()
+ * An array of token identifiers. Each individual token identifier is either a
+ * single character (i.e.: ;, ., >, !, etc...), or a three element array
+ * containing the token index in element 0, the string content of the original
+ * token in element 1 and the line number in element 2.
+ ******************************************************************************
+ */
+
+
+/**
+ * T_ML_COMMENT does not exist in PHP 5.
+ * The following three lines define it in order to
+ * preserve backwards compatibility.
+ *
+ * The next two lines define the PHP 5 only T_DOC_COMMENT,
+ * which we will mask as T_ML_COMMENT for PHP 4.
+ */
+if (!defined('T_ML_COMMENT')) {
+    define('T_ML_COMMENT', T_COMMENT);
+} else {
+    define('T_DOC_COMMENT', T_ML_COMMENT);
+}
+
+
+/******************************************************************************
+ * Functions
+ */
+
+
+/**
+ * @param $infile
+ *
+ * Pulled this example from php.net
+ */
+function strip_comments($infile)
+{
+    $source = file_get_contents($infile);
+    $tokens = token_get_all($source);
+
+    foreach ($tokens as $token) {
+        if (is_string($token)) {
+            // simple 1-character token
+            echo $token;
+        } else {
+            // token array
+            list($id, $text) = $token;
+
+            switch ($id) {
+                case T_COMMENT:
+                case T_ML_COMMENT: // we've defined this
+                case T_DOC_COMMENT: // and this
+                    // no action on comments
+                    break;
+
+                default:
+                    // anything else -> output "as is"
+                    echo $text;
+                    break;
+            }
+        }
+    }
+}
+
+
+/**
+ * @param $infile
+ *
+ * TODO: add token processing, still need to think this through :)
+ */
+function get_all_tokens($infile)
+{
+    $source = file_get_contents($infile);
+    $tokens = token_get_all($source);
+
+    echo "[*] Reading file: ", $infile, PHP_EOL;
+
+    foreach ($tokens as $token) {
+        if (is_string($token)) {
+            echo "[*] token is string: ", $token, PHP_EOL;
+        } else {
+            list($id, $text, $line) = $token;
+            echo "[*] line: ", $line, " id: ", $id, " text: ", $text, PHP_EOL;
+        }
+    }
+}
+
+
+/******************************************************************************
+ * Main Program
+ */
+
+// get the file to inspect
+// TODO: allow diretories and a list of file extensions
+$infile = $argv[1];
+
+// TODO: verify user input, does the file/directory exist
+
+// call each functions
+//strip_comments($infile);
+get_all_tokens($infile);
+
+?>
---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This is a test file that phpsec.php will attempt to parse.
+ *
+ * Naturally, we'll start small and build.
+ */
+
+printf("==================================================\n");
+printf("\n");
+printf("  Welcome to the PHPSecurity Testing Suite\n");
+printf("\n");
+printf("==================================================\n");
+
+// Create some constants
+define('APP_VERSION', '0.1-alpha');
+define('APP_NAME', 'PHPSecurity Review');
+
+// Create some variables
+$i = 0;
+$j = 10;
+$k = 100;
+
+printf("[*] i, j, k: $i, $j, $k\n");
+
+
+?>
